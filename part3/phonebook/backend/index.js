cf4c6152d9626958cda3731bc532fa5814398af1
Vili Rauhala <vili.rauhala@helsinki.fi>
2025-07-14 13:19:44 +0300

    For deploy

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
+// delete before deploy
+const cors = require('cors')
+app.use(cors())
+// ---
+
 app.use(express.json())
+app.use(morgan('tiny'))
 
 let persons = [
   {
@@ -78,7 +85,7 @@ app.post('/api/persons', (request, response) => {
     name: body.name,
     number: body.number
   }
-
+  console.log(body)
   persons = persons.concat(person)
 
   response.json(person)
@@ -91,7 +98,7 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
---
+++ part3/phonebook/frontend/src/App.jsx
@@ -0,0 +1,171 @@
+import { useState, useEffect } from 'react'
+import Notification from './components/Notification'
+import phonebookService from './services/persons'
+
+const Filter = ({ newSearch, handleSearchChange}) => (
+  <div>
+    filter shown with 
+    <input value={newSearch} onChange={handleSearchChange}/>
+  </div>
+)
+
+const PersonForm = (props) => (
+  <form onSubmit={props.addPerson}>
+    <div>
+      name: <input value={props.newName} onChange={props.handleNameChange}/>
+    </div>
+    <div>
+      number: <input value={props.newNumber} onChange={props.handleNumberChange}/>
+    </div>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form>
+)
+
+const Persons = (props) => (
+  <div>
+    {props.filtered.map(p => 
+      <Person key={p.name} person={p} handleRemove={props.handleRemove}></Person>
+    )}
+  </div>
+)
+
+const Person = ({ person, handleRemove }) => {
+  return (
+  <div>
+    {person.name} {person.number}
+    <button type="button" onClick={() => handleRemove(person)}>delete</button>
+  </div>
+)}
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [newSearch, setNewSearch] = useState('')
+  const [notificationMessage, setNotificationMessage] = useState({
+    message: null,
+    type: 'info'
+  })
+
+  useEffect(() => {
+    phonebookService
+      .getAll()
+      .then(response => {
+        setPersons(response)
+      })
+  }, [])
+
+  const clearNotification = () => {
+    setTimeout(() => setNotificationMessage({ message: null, type: 'info' }), 4000)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      number: newNumber,
+    }
+    if (!nameExists()) {
+      phonebookService
+        .create(personObject)
+        .then(returned => {
+          setPersons(persons.concat(returned))
+          setNotificationMessage(prev => ({
+            ...prev,
+            message: `Added ${personObject.name}`
+          }))
+          clearNotification()
+          setNewName('')
+          setNewNumber('')
+        })
+        .catch(error => {
+          console.log(error, "fail")
+        })
+    } else {
+      if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
+        const id = persons.find(p => p.name === newName)?.id
+        phonebookService
+          .update(id, personObject)
+          .then(returned => {
+            setPersons(persons.map(p =>
+              p.id == id ? { ...p, number: newNumber } : p
+            ))
+            setNotificationMessage({
+              message: `Updated ${personObject.name}`,
+              type: 'info'
+            })
+            clearNotification()
+            setNewName('')
+            setNewNumber('')
+          })
+          .catch(error => {
+            setNotificationMessage({
+              message: `Information of ${personObject.name} has already been removed from server`,
+              type: 'error'
+            })
+            clearNotification()
+          })
+      }
+    }
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleSearchChange = (event) => {
+    setNewSearch(event.target.value)
+  }
+
+  const handleRemove = (person) => {
+    if (window.confirm(`Delete ${person.name} ?`)) {
+    phonebookService
+      .remove(person.id)
+      .then(returned => {
+        setPersons(persons.filter(p => p.id !== person.id))
+      })
+    } else {
+      return
+    }
+  }
+
+  const nameExists = () => {
+    const condition = persons.some(p => p.name === newName)
+    return (condition)
+  }
+
+  const filterName = persons.filter(p =>
+    p.name.toLowerCase().includes(newSearch.toLowerCase())
+  )
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      
+      <Notification message={notificationMessage.message} notificationType={notificationMessage.type}/>
+      <Filter newSearch={newSearch} handleSearchChange={handleSearchChange}/>
+      
+      <h3>add a new</h3>
+
+      <PersonForm 
+        addPerson={addPerson}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
+      
+      <h2>Numbers</h2>
+
+      <Persons filtered={filterName} handleRemove={handleRemove}/>
+    </div>
+  )
+}
+
+export default App
